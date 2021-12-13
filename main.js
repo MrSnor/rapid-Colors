00a25edc0da1f65648421b49cc2e1acf7bdfd3b0
MrSnor <parthap.das36@gmail.com>
2021-12-13 10:04:18 +0530

    added and fixed alert text, added click event

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 
-alert("Flashing lights WARNING!!\n\n PLEASE DONT enter if have trouble with FLASHING LIGHTS")
-alert("Only enter if you're sure")
+alert("Flashing lights WARNING!!\n\nPLEASE DONT enter if you have trouble with FLASHING LIGHTS")
+alert("Instruction: \n\n(On mobile devices) Tap and swipe the screen to change the colors \n\n(On computers) Click or move the cursor")
 
+// function to get random values from 0 to 250
 function rdnum() {
     const roundRandNum = Math.floor(Math.random() * 251)
     return roundRandNum
 }
 
+// function to change background color
 function coloChange(e) {
     document.body.style.backgroundColor = `rgb(${rdnum()},${rdnum()},${rdnum()})`;
 }
 
+// listening to cursor events
 document.body.addEventListener("mousemove", coloChange);
+document.body.addEventListener("click", coloChange);
 
+// listening to touch events
 document.body.addEventListener("touchmove", coloChange);
